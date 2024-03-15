500d6995e6ba0112d1436ca5d84c264f4f631ef1
Álvaro <alvarohdezex@gmail.com>
2024-03-15 12:31:28 +0100

    feat: evaluation integer literals

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/aldezex/monkey/evaluator"
 	"github.com/aldezex/monkey/lexer"
 	"github.com/aldezex/monkey/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
